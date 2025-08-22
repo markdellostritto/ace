ee97902b530521281a1c465c0b2277e53598ed6f
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2025-08-22 11:12:42 -0400

    Added serialization

--- src/sim/constraint_factory.cpp
+++ src/sim/constraint_factory.cpp
@@ -5,6 +5,8 @@
 // constraints
 #include "sim/constraint_freeze.hpp"
 
+//==== constructors ====
+
 std::shared_ptr<Constraint> make_constraint(const Constraint::Name& name){
     std::shared_ptr<Constraint> ptr;
     switch(name){
@@ -17,6 +19,19 @@ std::shared_ptr<Constraint> make_constraint(const Constraint::Name& name){
     }
     return ptr;
 }
+
+std::shared_ptr<Constraint> copy(const std::shared_ptr<Constraint>& ptr){
+    switch(ptr->name()){
+        case Constraint::Name::FREEZE:{
+            return std::make_shared<ConstraintFreeze>(static_cast<const ConstraintFreeze&>(*ptr));
+        } break;
+        default:{
+            throw std::invalid_argument("make_constraint(const Constraint::Name&): Invalid constraint.");
+        }
+    }
+}
+
+//==== reading ====
 
 std::shared_ptr<Constraint>& read_constraint(std::shared_ptr<Constraint>& calc, Token& token){
     const Constraint::Name name=Constraint::Name::read(string::to_upper(token.next()).c_str());
@@ -32,10 +47,57 @@ std::shared_ptr<Constraint>& read_constraint(std::shared_ptr<Constraint>& calc, 
     return calc;
 }
 
+//==== output ====
+
 std::ostream& operator<<(std::ostream& out, const std::shared_ptr<Constraint>& calc){
     switch(calc->name()){
         case Constraint::Name::FREEZE: out<<static_cast<const Constraint&>(*calc); break;
         default: break;
     }
     return out;
+}
+
+//==== memory ====
+
+namespace serialize{
+
+    template <> int nbytes(const std::shared_ptr<Constraint>& ptr){
+        int size=0;
+        size+=nbytes(ptr->name());
+        switch(ptr->name()){
+            case Constraint::Name::FREEZE:{
+                size+=nbytes(static_cast<const ConstraintFreeze&>(*ptr));
+            }break;
+            default: break;
+        }
+        return size;
+    }
+
+    template <> int pack(const std::shared_ptr<Constraint>& ptr, char* arr){
+        int pos=0;
+        Constraint::Name name=ptr->name();
+        pos+=pack(name,arr+pos);
+        switch(name){
+            case Constraint::Name::FREEZE:{
+                pos+=pack(static_cast<const ConstraintFreeze&>(*ptr),arr+pos);
+            }break;
+            default: break;
+        }
+        return pos;
+    }
+
+    template <> int unpack(std::shared_ptr<Constraint>& ptr, const char* arr){
+        int pos=0;
+        Constraint::Name name=Constraint::Name::NONE;
+        pos+=unpack(name,arr+pos);
+        switch(name){
+            case Constraint::Name::FREEZE:{
+                ptr=std::make_shared<ConstraintFreeze>();
+                pos+=unpack(static_cast<ConstraintFreeze&>(*ptr),arr+pos);
+            }break;
+            default: break;
+        }
+        return pos;
+    }
+
 }
--- src/sim/constraint_factory.hpp
+++ src/sim/constraint_factory.hpp
@@ -7,8 +7,28 @@
 // sim
 #include "sim/constraint.hpp"
 
+//==== constructors ====
+
 std::shared_ptr<Constraint> make_constraint(const Constraint::Name& name);
+std::shared_ptr<Constraint> copy(const std::shared_ptr<Constraint>& ptr);
+
+//==== reading ====
+
 std::shared_ptr<Constraint>& read_constraint(std::shared_ptr<Constraint>& pot, Token& token);
+
+//==== output ====
+
 std::ostream& operator<<(std::ostream& out, const std::shared_ptr<Constraint>& constraint);
+
+//==== memory ====
+
+namespace serialize{
+
+    template <> int nbytes(const std::shared_ptr<Constraint>& ptr);
+
+    template <> int pack(const std::shared_ptr<Constraint>& ptr, char* arr);
+
+    template <> int unpack(std::shared_ptr<Constraint>& ptr, const char* arr);
+}
 
 #endif
