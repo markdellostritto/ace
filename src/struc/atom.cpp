d4383cc73facb5f9c35c55f5a3dc38eb62159a5b
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2025-12-01 15:41:09 -0500

    Changed storage method for atom

--- src/struc/atom.cpp
+++ src/struc/atom.cpp
@@ -13,66 +13,48 @@
 
 std::ostream& operator<<(std::ostream& out, const Atom& atom){
 	//basic properties
-	if(atom.name)   out<<"name ";
-	if(atom.an)     out<<"an ";
-	if(atom.type)   out<<"type ";
+	if(atom.name())   out<<"name ";
+	if(atom.an())     out<<"an ";
+	if(atom.type())   out<<"type ";
 	//serial properties
-	if(atom.mass)   out<<"mass ";
-	if(atom.charge) out<<"charge ";
-	if(atom.radius) out<<"radius ";
-	if(atom.eta)    out<<"eta ";
+	if(atom.mass())   out<<"mass ";
+	if(atom.charge()) out<<"charge ";
+	if(atom.radius()) out<<"radius ";
+	if(atom.eta())    out<<"eta ";
 	//vector properties
-	if(atom.image)  out<<"image ";
-	if(atom.posn)   out<<"posn ";
-	if(atom.vel)    out<<"vel ";
-	if(atom.force)  out<<"force ";
+	if(atom.image())  out<<"image ";
+	if(atom.posn())   out<<"posn ";
+	if(atom.vel())    out<<"vel ";
+	if(atom.force())  out<<"force ";
+	if(atom.dipole()) out<<"dipole ";
 	//symm
-	if(atom.symm)	out<<"symm ";
+	if(atom.symm())	out<<"symm ";
 	//return
 	return out;
 }
 
-void Atom::defaults(){
-	//basic properties
-	name   = false;
-	an     = false;
-	type   = false;
-	//serial properties
-	mass   = false;
-	charge = false;
-	radius = false;
-	eta    = false;
-	//vector properties
-	image  = false;
-	posn   = false;
-	vel    = false;
-	force  = false;
-	//nnp
-	symm   = false;
-}
-
-Atom Atom::read(Token& token){
-	Atom atom;
+Atom& Atom::read(Token& token){
 	while(!token.end()){
 		const std::string tag=string::to_upper(token.next());
 		//basic properties
-		if(tag=="NAME") atom.name=true;
-		else if(tag=="AN") atom.an=true;
-		else if(tag=="TYPE") atom.type=true;
+		if(tag=="NAME") name()=true;
+		else if(tag=="AN") an()=true;
+		else if(tag=="TYPE") type()=true;
 		//serial properties
-		else if(tag=="MASS") atom.mass=true;
-		else if(tag=="CHARGE") atom.charge=true;
-		else if(tag=="RADIUS") atom.radius=true;
-		else if(tag=="ETA") atom.eta=true;
+		else if(tag=="MASS") mass()=true;
+		else if(tag=="CHARGE") charge()=true;
+		else if(tag=="RADIUS") radius()=true;
+		else if(tag=="ETA") eta()=true;
 		//vector properties
-		else if(tag=="IMAGE") atom.image=true;
-		else if(tag=="POSN") atom.posn=true;
-		else if(tag=="VEL") atom.vel=true;
-		else if(tag=="FORCE") atom.force=true;
+		else if(tag=="IMAGE") image()=true;
+		else if(tag=="POSN") posn()=true;
+		else if(tag=="VEL") vel()=true;
+		else if(tag=="FORCE") force()=true;
+		else if(tag=="DIPOLE") dipole()=true;
 		//nnp
-		else if(tag=="SYMM") atom.symm=true;
+		else if(tag=="SYMM") symm()=true;
 	}
-	return atom;
+	return *this;
 }
 
 //**********************************************************************************************
@@ -86,25 +68,7 @@ namespace serialize{
 	//**********************************************
 	
 	template <> int nbytes(const Atom& obj){
-		int size=0;
-		//basic properties
-		size+=sizeof(obj.name);
-		size+=sizeof(obj.an);
-		size+=sizeof(obj.type);
-		//serial properties
-		size+=sizeof(obj.mass);
-		size+=sizeof(obj.charge);
-		size+=sizeof(obj.radius);
-		size+=sizeof(obj.eta);
-		//vector properties
-		size+=sizeof(obj.image);
-		size+=sizeof(obj.posn);
-		size+=sizeof(obj.vel);
-		size+=sizeof(obj.force);
-		//nnp
-		size+=sizeof(obj.symm);
-		//return
-		return size;
+		return Atom::SIZE*sizeof(bool);
 	}
 	
 	//**********************************************
@@ -113,23 +77,7 @@ namespace serialize{
 	
 	template <> int pack(const Atom& obj, char* arr){
 		int pos=0;
-		//basic properties
-		std::memcpy(arr+pos,&obj.name,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.an,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.type,sizeof(bool)); pos+=sizeof(bool);
-		//serial properties
-		std::memcpy(arr+pos,&obj.mass,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.charge,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.radius,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.eta,sizeof(bool)); pos+=sizeof(bool);
-		//vector properties
-		std::memcpy(arr+pos,&obj.image,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.posn,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.vel,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(arr+pos,&obj.force,sizeof(bool)); pos+=sizeof(bool);
-		//nnp
-		std::memcpy(arr+pos,&obj.symm,sizeof(bool)); pos+=sizeof(bool);
-		//return
+		std::memcpy(arr+pos,obj.data().data(),Atom::SIZE*sizeof(bool)); pos+=Atom::SIZE*sizeof(bool);
 		return pos;
 	}
 	
@@ -139,23 +87,7 @@ namespace serialize{
 	
 	template <> int unpack(Atom& obj, const char* arr){
 		int pos=0;
-		//basic properties
-		std::memcpy(&obj.name,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.an,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.type,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		//serial properties
-		std::memcpy(&obj.mass,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.charge,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.radius,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.eta,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		//vector properties
-		std::memcpy(&obj.image,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.posn,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.vel,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		std::memcpy(&obj.force,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		//nnp
-		std::memcpy(&obj.symm,arr+pos,sizeof(bool)); pos+=sizeof(bool);
-		//return
+		std::memcpy(obj.data().data(),arr+pos,Atom::SIZE*sizeof(bool)); pos+=Atom::SIZE*sizeof(bool);
 		return pos;
 	}
 	
--- src/struc/atom.hpp
+++ src/struc/atom.hpp
@@ -4,6 +4,7 @@
 
 // c++ libraries
 #include <iosfwd>
+#include <array>
 // mem
 #include "mem/serialize.hpp"
 // str
@@ -13,35 +14,69 @@
 //Atom
 //**********************************************************************************************
 
-struct Atom{
-	//==== data ====
-	//coordinates
-	bool frac;
-	//basic properties
-	bool name;
-	bool an;
-	bool type;
-	//serial properties
-	bool mass;
-	bool charge;
-	bool radius;
-	bool eta;
-	//vector properties
-	bool image;
-	bool posn;
-	bool vel;
-	bool force;
-	//nnp
-	bool symm;
+class Atom{
+public:
+	static const int SIZE=14;
+private:
+	std::array<bool,SIZE> data_;
+public:
+	
 	//==== constructors/destructors ====
-	Atom(){defaults();}
+	Atom(){clear();}
 	~Atom(){}
+
 	//==== operators ====
 	friend std::ostream& operator<<(std::ostream& out, const Atom& atom);
+
+	//==== access - mutable ====
+	//data
+	std::array<bool,14>& data(){return data_;}
+	//coordinates
+	bool& frac(){return data_[0];} 
+	//basic properties
+	bool& name(){return data_[1];} 
+	bool& an(){return data_[2];} 
+	bool& type(){return data_[3];} 
+	//serial properties
+	bool& mass(){return data_[4];} 
+	bool& charge(){return data_[5];} 
+	bool& radius(){return data_[6];} 
+	bool& eta(){return data_[7];} 
+	//vector properties
+	bool& image(){return data_[8];} 
+	bool& posn(){return data_[9];} 
+	bool& vel(){return data_[10];} 
+	bool& force(){return data_[11];} 
+	bool& dipole(){return data_[12];} 
+	//nnp
+	bool& symm(){return data_[13];} 
+
+	//==== access - const ====
+	//data
+	const std::array<bool,14>& data()const{return data_;}
+	//coordinates
+	const bool& frac()const{return data_[0];} 
+	//basic properties
+	const bool& name()const{return data_[1];} 
+	const bool& an()const{return data_[2];} 
+	const bool& type()const{return data_[3];} 
+	//serial properties
+	const bool& mass()const{return data_[4];} 
+	const bool& charge()const{return data_[5];} 
+	const bool& radius()const{return data_[6];} 
+	const bool& eta()const{return data_[7];} 
+	//vector properties
+	const bool& image()const{return data_[8];} 
+	const bool& posn()const{return data_[9];} 
+	const bool& vel()const{return data_[10];} 
+	const bool& force()const{return data_[11];} 
+	const bool& dipole()const{return data_[12];} 
+	//nnp
+	const bool& symm()const{return data_[13];} 
+
 	//==== member functions ====
-	void defaults();
-	void clear(){defaults();}
-	static Atom read(Token& token);
+	void clear(){data_.fill(false);}
+	Atom& read(Token& token);
 };
 
 //**********************************************************************************************
